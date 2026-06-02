c7769e20784aafe6cbd6bd19fa4ffaefced244c1
Hariom Patel <hariom70002582@gmail.com>
2026-06-02 08:51:47 +0530

    refactor: remove authentication-related code and simplify App component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css'
-import { BrowserRouter } from 'react-router-dom'
-import AppRoutes from './routes/AppRoutes'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app-container">
-        <AppRoutes />
-      </div>
-    </BrowserRouter>
+    <>
+      <h1>Hello World</h1>
+    </>
   )
 }
 
